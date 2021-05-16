37b13768508cbd1df0bc9bce77eade270174b29b
XIU-maker <58578449+XIU-maker@users.noreply.github.com>
2021-05-16 16:15:32 +0900

    Add files via upload

--- ENJcoin_xiu.py
+++ ENJcoin_xiu.py
@@ -56,14 +56,14 @@ while True:
             current_price = get_current_price("KRW-ENJ")
             if target_price < current_price:
                 krw = get_balance("KRW")
-                if krw > 1000:
+                if krw > 7000000:
                     upbit.buy_market_order("KRW-ENJ", krw*0.69)
             elif target_row1 > current_price:
                 krw = get_balance("KRW")
                 if target_row2 > current_price:
                     if krw > 1000:
                         upbit.buy_market_order("KRW-ENJ", krw*0.9995)
-                elif krw > 1000:
+                elif krw > 7000000:
                     upbit.buy_market_order("KRW-ENJ", krw*0.5)
                 
         else:
