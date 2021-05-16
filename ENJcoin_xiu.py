81e3e6a52c75a4c8e236397d9547144552bf34b5
XIU-maker <58578449+XIU-maker@users.noreply.github.com>
2021-05-16 12:12:05 +0900

    Update ENJcoin_xiu.py

--- ENJcoin_xiu.py
+++ ENJcoin_xiu.py
@@ -5,35 +5,18 @@ import datetime
 access = "IcQa6tEhx2B716i6Hwxy2Rl8t2rFRtlVNXhHwxns"
 secret = "hOixhxPXEK3UIvhWBvM5CTKLgeNvRhWrTeZiDzlN"
 
-def get_target_price_ENJ(ticker, k):
+def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
     df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
     target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
     return target_price
 
-def get_target_price_CRE(ticker, c):
+def get_target_row(ticker, r):
     """변동성 돌파 전략으로 매수 목표가 조회"""
     df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
-    target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * c
-    return target_price
-
-# def get_target_price_TSHP(ticker, t):
-#     """변동성 돌파 전략으로 매수 목표가 조회"""
-#     df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
-#     target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * t
-#     return target_price
-
-def get_target_row1_ENJ(ticker, l):
-    """변동성 돌파 전략으로 매수 목표가 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
-    target_row1 = df.iloc[0]['close'] * l
+    target_row1 = df.iloc[0]['close'] * r
     return target_row1
 
-def get_target_row2_ENJ(ticker, j):
-    """변동성 돌파 전략으로 매수 목표가 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
-    target_row2 = df.iloc[0]['close'] * j
-    return target_row2
 
 def get_start_time(ticker):
     """시작 시간 조회"""
@@ -68,12 +51,14 @@ while True:
 
         # 9:00 < 현재 < #8:00:00
         if start_time < now < end_time - datetime.timedelta(seconds=3600):
-            target_price_ENJ = get_target_price_ENJ("KRW-ENJ", 0.5)
-            target_price_CRE = get_target_price_CRE("KRW-CRE", 0.5)
-            target_row1_ENJ = get_target_row1_ENJ("KRW-ENJ", 0.82)
-            target_row2_ENJ = get_target_row2_ENJ("KRW-ENJ", 0.72)
+            target_price_ENJ = get_target_price("KRW-ENJ", 0.5)
+            target_price_CRE = get_target_price("KRW-CRE", 0.5)
+            target_price_TSHP = get_target_price("KRW-TSHP", 0.5)
+            target_row1_ENJ = get_target_row("KRW-ENJ", 0.82)
+            target_row2_ENJ = get_target_row("KRW-ENJ", 0.72)
             current_price_ENJ = get_current_price("KRW-ENJ")
             current_price_CRE = get_current_price("KRW-CRE")
+            current_price_TSHP = get_current_price("KRW-TSHP")
             if target_price_ENJ < current_price_ENJ:
                 krw = get_balance("KRW")
                 if krw > 1000:
@@ -90,14 +75,22 @@ while True:
                 krw = get_balance("KRW")
                 if krw > 1000:
                     upbit.buy_market_order("KRW-CRE", krw*0.69)
+
+            elif target_price_TSHP < current_price_TSHP:
+                krw = get_balance("KRW")
+                if krw > 1000:
+                    upbit.buy_market_order("KRW-TSHP", krw*0.69)
                 
         else:
             ENJ = get_balance("ENJ")
             CRE = get_balance("CRE")
+            TSHP = get_balance("TSHP")
             if ENJ > 0.00009:
                 upbit.sell_market_order("KRW-ENJ", ENJ*0.9995)
             elif CRE > 0.00009:
                 upbit.sell_market_order("KRW-CRE", CRE*0.9995)
+            elif TSHP > 0.00009:
+                upbit.sell_market_order("KRW-TSHP", TSHP*0.9995)
         time.sleep(1)
     except Exception as e:
 
