02495479b5d668a389fc4ec524b2a83ebb42d12c
XIU-maker <58578449+XIU-maker@users.noreply.github.com>
2022-07-27 20:44:21 +0900

    Update XEMcoin_xiu.py

--- XEMcoin_xiu.py
+++ XEMcoin_xiu.py
@@ -45,8 +45,8 @@ while True:
         # xem = get_balance("XEM")
         
         if start_time < now < end_time - datetime.timedelta(seconds=3600):
-            target_price = get_target_price("KRW-XEM", 0.06)
-            target_high = get_target_price("KRW-XEM", 0.1)
+            target_price = get_target_price("KRW-XEM", 0.6)
+            target_high = get_target_price("KRW-XEM", 1.0)
             current_price = get_current_price("KRW-XEM")
             xem = get_balance("XEM")
             if target_price < current_price and xem == 0:
