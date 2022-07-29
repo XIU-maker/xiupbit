f9fafaf0ae031c6c9a2019703faef33a171a1977
XIU-maker <58578449+XIU-maker@users.noreply.github.com>
2022-07-29 10:52:04 +0900

    Update BSVcoin_xiu.py

--- BSVcoin_xiu.py
+++ BSVcoin_xiu.py
@@ -46,7 +46,7 @@ while True:
         
         if start_time < now < end_time - datetime.timedelta(seconds=3600):
             target_price = get_target_price("KRW-BSV", 0.5)
-            target_high = get_target_price("KRW-BSV", 1.0)
+            target_high = get_target_price("KRW-BSV", 0.8)
             current_price = get_current_price("KRW-BSV")
             bsv = get_balance("BSV")
             if target_price < current_price and bsv == 0:
