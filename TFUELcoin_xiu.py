09be35bc4fd3957be65cb1e7bdb13618b08fed5d
XIU-maker <58578449+XIU-maker@users.noreply.github.com>
2022-08-01 18:28:14 +0900

    Add files via upload

--- TFUELcoin_xiu.py
+++ TFUELcoin_xiu.py
@@ -2,8 +2,8 @@ import time
 import pyupbit
 import datetime
 
-access = "IcQa6tEhx2B716i6Hwxy2Rl8t2rFRtlVNXhHwxns"
-secret = "hOixhxPXEK3UIvhWBvM5CTKLgeNvRhWrTeZiDzlN"
+access = "vqTw2OoiMI2JSP01nmU55W8vt1UPkhWMA3gEW5QS"
+secret = "spVx1FijHfWtmHjguh3zBanxYvpnm5k0PrsBec8K"
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
@@ -26,10 +26,11 @@ def get_balance(ticker):
                 return float(b['balance'])
             else:
                 return 0
+    return 0
 
 def get_current_price(ticker):
     """현재가 조회"""
-    return pyupbit.get_orderbook(tickers=ticker)[0]["orderbook_units"][0]["ask_price"]
+    return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]
 
 # 로그인
 upbit = pyupbit.Upbit(access, secret)
@@ -39,22 +40,28 @@ print("autotrade start")
 while True:
     try:
         now = datetime.datetime.now()
-        start_time = get_start_time("KRW-BTC") #9:00
-        end_time = start_time + datetime.timedelta(days=1) #9:00 + 1일
-
-        # 9:00 < 현재 < #8:59:50
+        start_time = get_start_time("KRW-TFUEL")
+        end_time = start_time + datetime.timedelta(days=1)
+        # tfuel = get_balance("TFUEL")
+        
         if start_time < now < end_time - datetime.timedelta(seconds=3600):
-            target_price = get_target_price("KRW-TFUEL", 0.3)
+            target_price = get_target_price("KRW-TFUEL", 0.8)
+            target_high = get_target_price("KRW-TFUEL", 1.5)
             current_price = get_current_price("KRW-TFUEL")
-            if target_price < current_price:
+            tfuel = get_balance("TFUEL")
+            if target_price < current_price and tfuel == 0:
                 krw = get_balance("KRW")
-                if krw > 1000:
+                if krw > 5000:
                     upbit.buy_market_order("KRW-TFUEL", krw*0.9995)
+            elif target_high < current_price and tfuel > 100:
+                 upbit.sell_market_order("KRW-TFUEL", tfuel*0.9995)
+                
+
         else:
             tfuel = get_balance("TFUEL")
-            if tfuel > 0.001:
+            if tfuel > 0.8:
                 upbit.sell_market_order("KRW-TFUEL", tfuel*0.9995)
-        time.sleep(10)
+        time.sleep(18)
     except Exception as e:
         print(e)
-        time.sleep(10)
+        time.sleep(18)
