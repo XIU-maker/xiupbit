120896fb274b3d458586d5fb2e5c6678f84801a2
XIU-maker <58578449+XIU-maker@users.noreply.github.com>
2022-08-02 20:48:53 +0900

    Update ENJcoin_xiu.py

--- ENJcoin_xiu.py
+++ ENJcoin_xiu.py
@@ -45,8 +45,8 @@ while True:
         # ENJ = get_balance("ENJ")
         
         if start_time < now < end_time - datetime.timedelta(seconds=3600):
-            target_price = get_target_price("KRW-ENJ", 0.4)
-            target_high = get_target_price("KRW-ENJ", 0.7)
+            target_price = get_target_price("KRW-ENJ", 0.38)
+            target_high = get_target_price("KRW-ENJ", 0.55)
             current_price = get_current_price("KRW-ENJ")
             enj = get_balance("ENJ")
             if target_price < current_price and enj == 0:
