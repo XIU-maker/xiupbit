801aa4325a11dac11801e9d3873663ab0dc85957
XIU-maker <58578449+XIU-maker@users.noreply.github.com>
2022-08-02 12:32:51 +0900

    Update SBDcoin_xiu.py

--- SBDcoin_xiu.py
+++ SBDcoin_xiu.py
@@ -45,8 +45,8 @@ while True:
         # sbd = get_balance("sbd")
         
         if start_time < now < end_time - datetime.timedelta(seconds=3600):
-            target_price = get_target_price("KRW-SBD", 0.5)
-            target_high = get_target_price("KRW-SBD", 0.8)
+            target_price = get_target_price("KRW-SBD", 0.4)
+            target_high = get_target_price("KRW-SBD", 0.6)
             current_price = get_current_price("KRW-SBD")
             sbd = get_balance("SBD")
             if target_price < current_price and sbd == 0:
