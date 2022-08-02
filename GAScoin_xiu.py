c9e23675425e1ff868ff91335ffc7d525c385900
XIU-maker <58578449+XIU-maker@users.noreply.github.com>
2022-08-02 20:48:26 +0900

    Update GAScoin_xiu.py

--- GAScoin_xiu.py
+++ GAScoin_xiu.py
@@ -45,8 +45,8 @@ while True:
         # gas = get_balance("gas")
         
         if start_time < now < end_time - datetime.timedelta(seconds=3600):
-            target_price = get_target_price("KRW-GAS", 0.4)
-            target_high = get_target_price("KRW-GAS", 0.6)
+            target_price = get_target_price("KRW-GAS", 0.38)
+            target_high = get_target_price("KRW-GAS", 0.53)
             current_price = get_current_price("KRW-GAS")
             gas = get_balance("GAS")
             if target_price < current_price and gas == 0:
