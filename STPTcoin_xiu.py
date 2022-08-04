1d7cca95aa5629b6cf8a2e29b2ff998a1b2b51d0
XIU-maker <58578449+XIU-maker@users.noreply.github.com>
2022-08-04 20:07:29 +0900

    Update STPTcoin_xiu.py

--- STPTcoin_xiu.py
+++ STPTcoin_xiu.py
@@ -45,7 +45,7 @@ while True:
         # stpt = get_balance("stpt")
         
         if start_time < now < end_time - datetime.timedelta(seconds=3600):
-            target_price = get_target_price("KRW-STPT", 0.33)
+            target_price = get_target_price("KRW-STPT", 0.38)
             target_high = get_target_price("KRW-STPT", 0.53)
             current_price = get_current_price("KRW-STPT")
             stpt = get_balance("STPT")
